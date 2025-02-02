36af2b5bb7e8512f892aec1704a9703b4b315ca3
Boslodan <sjankovic01@gmail.com>
2025-02-02 17:09:47 +0100

    KreiranjeNiti

---
+++ SeminarskiServer/src/niti/Nit.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package niti;
+
+import db.DBbroker;
+import domain.Proizvod;
+import domain.RadnaSmena;
+import domain.Radnik;
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.List;
+import komunikacija.Odgovor;
+import komunikacija.Operacija;
+import komunikacija.Receiver;
+import komunikacija.Sender;
+import komunikacija.Zahtev;
+
+/**
+ *
+ * @author Slobodan
+ */
+public class Nit implements Runnable{
+
+    private Socket soket;
+    private DBbroker dbb;
+    private Receiver receiver;
+    private Sender sender;
+
+    public Nit(Socket soket, DBbroker dbb) {
+        this.soket = soket;
+        this.dbb = dbb;
+        receiver = new Receiver(soket);
+        sender = new Sender(soket);
+    }
+    
+    
+    @Override
+    public void run() {
+        
+        try {
+            while (true) {
+                try {
+                    // Čitaj zahtev
+                    Zahtev zahtev = (Zahtev) receiver.receive();
+                    Odgovor odgovor = new Odgovor();
+
+                    Operacija operacija = zahtev.getOperacija();
+                    if (operacija == Operacija.LOGIN) {
+                        Radnik radnik = (Radnik) zahtev.getArgumenti();
+                        try {
+                            radnik = dbb.getRadnik(radnik);
+                            odgovor.setResult(radnik);
+                        } catch (SQLException e) {
+                            odgovor.setEx(e);
+                        }
+                    } else if (operacija == Operacija.VRATI_PROIZVODE) {
+                        try {
+                            List<Proizvod> proizvodi = dbb.vratiListuSviProizvodi();
+                            odgovor.setResult(proizvodi);
+                        } catch (SQLException e) {
+                            odgovor.setEx(e);
+                        }
+                    } else if (operacija == Operacija.KREIRAJ_PROIZVOD) {
+                        try {
+                            if (dbb.kreirajProizvod((Proizvod) zahtev.getArgumenti())) {
+                                odgovor.setResult(true);
+                            }
+                        } catch (SQLException e) {
+                            odgovor.setEx(e);
+                        }
+                    } else if (operacija == Operacija.KREIRAJ_RADNUSMENU) {
+                        try {
+                            if (dbb.ubaciRadnuSmenu((RadnaSmena) zahtev.getArgumenti())) {
+                                odgovor.setResult(true);
+                            }
+                        } catch (Exception e) {
+                            odgovor.setEx(e);
+                        }
+                    } else if (operacija == Operacija.VRATI_RADNESMENE) {
+                        try {
+                            List<RadnaSmena> radneSmene = dbb.vratiListuSviRadnaSmena();
+                            odgovor.setResult(radneSmene);
+                        } catch (SQLException e) {
+                            odgovor.setEx(e);
+                        }
+                    } else if (operacija == Operacija.OBRISI_RADNUSMENU) {
+                        try {
+                            odgovor.setResult(dbb.obrisiRadnaSmena((int) zahtev.getArgumenti()));
+                        } catch (SQLException e) {
+                            odgovor.setEx(e);
+                        }
+                    }
+
+                    // Pošalji odgovor klijentu
+                    sender.send(odgovor);
+
+                } catch (Exception ex) {
+                    System.out.println("Greska "+ex.getMessage());
+                }
+            }
+        } finally {
+            try {
+                soket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+    
+    
+    
+    
+}
--- SeminarskiServer/src/server/Server.java
+++ SeminarskiServer/src/server/Server.java
@@ -5,22 +5,10 @@
 package server;
 
 import db.DBbroker;
-import domain.Proizvod;
-import domain.RadnaSmena;
-import domain.Radnik;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import komunikacija.Odgovor;
-import komunikacija.Operacija;
-import komunikacija.Receiver;
-import komunikacija.Sender;
-import komunikacija.Zahtev;
+import niti.Nit;
 
 /**
  *
@@ -29,13 +17,13 @@ import komunikacija.Zahtev;
 public class Server {
 
     private DBbroker dbb;
-    private Receiver receiver;
-    private Sender sender;
-
+    
+    
     public Server() {
         dbb = new DBbroker();
     }
 
+    
     public void startServer() {
 
         try {
@@ -43,84 +31,17 @@ public class Server {
             ServerSocket serverSoket = new ServerSocket(9000);
             System.out.println("Server pokrenut cekam klijenta");
 
-            //Cekanje klijenta
-            Socket soket = serverSoket.accept();
-            System.out.println("Server:Klijent se povezao sa serverom");
-
-            receiver = new Receiver(soket);
-            sender = new Sender(soket);
-
             while (true) {
-                try {
-                    //citaj zahtev
-                    Zahtev zahtev = (Zahtev) receiver.receive();
-                    Odgovor odgovor = new Odgovor();
-
-                    Operacija operacija = zahtev.getOperacija();
-                    if (operacija == Operacija.LOGIN) {
-                        Radnik radnik = (Radnik) zahtev.getArgumenti();
-                        try {
-                            radnik = dbb.getRadnik(radnik);
-                            odgovor.setResult(radnik);
-                        } catch (SQLException e) {
-                            odgovor.setEx(e);
-                        }
-                    } else if (operacija == Operacija.VRATI_PROIZVODE) {
-                        // Logika za vracanje proizvoda
-                        try {
-                            List<Proizvod> proizvodi = dbb.vratiListuSviProizvodi();
-                            odgovor.setResult(proizvodi);
-                        } catch (SQLException e) {
-                            odgovor.setEx(e);
-                        }
-                    } else if(operacija==Operacija.KREIRAJ_PROIZVOD){
-                        
-                        try {
-                            
-                            if(dbb.kreirajProizvod((Proizvod)zahtev.getArgumenti())){
-                                odgovor.setResult(true);
-                            }
-                        } catch (SQLException e) {
-                            odgovor.setEx(e);
-                        }   
-                    } else if(operacija==Operacija.KREIRAJ_RADNUSMENU){
-                        
-                        try {
-                            
-                            if(dbb.ubaciRadnuSmenu((RadnaSmena) zahtev.getArgumenti())){
-                                odgovor.setResult(true);
-                            }
-
-                        } catch (Exception e) {
-                            odgovor.setEx(e);
-                        }
-                    }else if (operacija == Operacija.VRATI_RADNESMENE) {
-                        
-                        try {
-                            List<RadnaSmena> radneSmene = dbb.vratiListuSviRadnaSmena();
-                            odgovor.setResult(radneSmene);
-                        } catch (SQLException e) {
-                            odgovor.setEx(e);
-                        }
-                    }else if (operacija == Operacija.OBRISI_RADNUSMENU) {
-                        
-                        try {
-                            odgovor.setResult(dbb.obrisiRadnaSmena((int)zahtev.getArgumenti()));
-                        } catch (SQLException e) {
-                            odgovor.setEx(e);
-                        }
-                    }  
-                    
-                    //posalji odgovor
-                    sender.send(odgovor);
-
-                } catch (Exception ex) {
-                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                //Cekanje klijenta
+                Socket soket = serverSoket.accept();
+                System.out.println("Server:Klijent se povezao sa serverom");
+                
+                Nit nit=new Nit(soket, dbb);
+                nit.run();
             }
 
         } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Greska"+ex.getMessage());
         }
 
     }
